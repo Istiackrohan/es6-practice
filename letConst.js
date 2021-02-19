ccbbc1dfda8f2e0fec2e45a0d5273be04b202fff
Istiackrohan <istiackrohan@gmail.com>
2021-02-19 21:23:23 +0600

    shikhtesi

---
+++ letConst.js
@@ -0,0 +1,25 @@
+// const er kaj
+
+const hubby = "Jaantus";
+console.log(hubby);
+
+const numbers = [12, 18];
+numbers[1] = 88;
+numbers.push(56);
+
+const nayok = {name:"Tom cruse", phone: 5623};
+
+// let er kaj
+
+let patientName = "Chengis khan";
+patientName = "Mr. Abdul Kuddus";
+console.log(patientName);
+let sum = 0;
+// for(var i = 0; i < 10; i++){
+//     sum = sum + i;
+// }
+// console.log(i);
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}                                   //let is leak proof
+console.log(i);
